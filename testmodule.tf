9454be9ff22d4847f552f8df8cf3317ef65019cd
anvgithub <88430353+anvgithub@users.noreply.github.com>
2021-09-17 17:56:20 +0300

    Update testmodule.tf

--- testmodule.tf
+++ testmodule.tf
@@ -22,46 +22,45 @@ skip_provider_registration = "true"
 }
 
 resource "azurerm_resource_group" "example" {
-  name     = "example-resources"
+  name     = "my-resources"
   location = "West Europe"
 }
 
-module "linuxservers" {
-  source              = "Azure/compute/azurerm"
+module "vnet" {
+  source              = "Azure/vnet/azurerm"
   resource_group_name = azurerm_resource_group.example.name
-  vm_os_simple        = "UbuntuServer"
-  public_ip_dns       = ["linsimplevmips"] // change to a unique name per datacenter region
-  vnet_subnet_id      = module.network.vnet_subnets[0]
+  address_space       = ["10.0.0.0/16"]
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
 
-  depends_on = [azurerm_resource_group.example]
+  nsg_ids = {
+    subnet1 = azurerm_network_security_group.ssh.id
+    subnet2 = azurerm_network_security_group.ssh.id
+    subnet3 = azurerm_network_security_group.ssh.id
+  }
+
+
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
 }
 
-module "windowsservers" {
-  source              = "Azure/compute/azurerm"
+resource "azurerm_network_security_group" "ssh" {
+  name                = "ssh"
   resource_group_name = azurerm_resource_group.example.name
-  is_windows_image    = true
-  vm_hostname         = "mywinvm" // line can be removed if only one VM module per resource group
-  admin_password      = "ComplxP@ssw0rd!"
-  vm_os_simple        = "WindowsServer"
-  public_ip_dns       = ["winsimplevmips"] // change to a unique name per datacenter region
-  vnet_subnet_id      = module.network.vnet_subnets[0]
+  location            = azurerm_resource_group.example.location
 
-  depends_on = [azurerm_resource_group.example]
-}
+  security_rule {
+    name                       = "test123"
+    priority                   = 100
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
 
-module "network" {
-  source              = "Azure/network/azurerm"
-  resource_group_name = azurerm_resource_group.example.name
-  subnet_prefixes     = ["10.0.1.0/24"]
-  subnet_names        = ["subnet1"]
-
-  depends_on = [azurerm_resource_group.example]
-}
-
-output "linux_vm_public_name" {
-  value = module.linuxservers.public_ip_dns_name
-}
-
-output "windows_vm_public_name" {
-  value = module.windowsservers.public_ip_dns_name
 }
