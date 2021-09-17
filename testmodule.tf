aaf35e40c2e2b1a0583fe54fce3c900e2539b9ba
anvgithub <88430353+anvgithub@users.noreply.github.com>
2021-09-17 17:18:48 +0300

    Update testmodule.tf

--- testmodule.tf
+++ testmodule.tf
@@ -1,10 +1,25 @@
+terraform {
+
+  required_version = ">=0.12"
+  
+  required_providers {
+    azurerm = {
+      source = "hashicorp/azurerm"
+      version = "~>2.0"
+    }
+  }
+
     backend "azurerm" {
         resource_group_name  = "system_resource_group"
         storage_account_name = "storageforsystemdata"
         container_name       = "tfstate"
         key                  = "terraform.tfstate"
     }
-
+}
+provider "azurerm" {
+skip_provider_registration = "true"
+  features {}
+}
 
 module "network" {
     source              = "Azure/network/azurerm"
