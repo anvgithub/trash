beb70604ba490db5d6df9947c6b792b235173c99
anvgithub <88430353+anvgithub@users.noreply.github.com>
2021-09-17 18:38:29 +0300

    Update testmodule.tf

--- testmodule.tf
+++ testmodule.tf
@@ -17,11 +17,30 @@ terraform {
     }
 }
 provider "azurerm" {
-skip_provider_registration = "true"
   features {}
 }
 
-resource "azurerm_resource_group" "vnet" {
+resource "azurerm_resource_group" "example" {
   name     = "my-resources"
   location = "West US"
+}
+
+module "vnet" {
+  source              = "Azure/vnet/azurerm"
+  resource_group_name = azurerm_resource_group.example.name
+  address_space       = ["10.0.0.0/16"]
+  subnet_prefixes     = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+  subnet_names        = ["subnet1", "subnet2", "subnet3"]
+
+  subnet_service_endpoints = {
+    subnet2 = ["Microsoft.Storage", "Microsoft.Sql"],
+    subnet3 = ["Microsoft.AzureActiveDirectory"]
+  }
+
+  tags = {
+    environment = "dev"
+    costcenter  = "it"
+  }
+
+  depends_on = [azurerm_resource_group.example]
 }
