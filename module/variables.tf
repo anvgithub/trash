7f454414f24a2b3587b5929eb1fd606ac8488a55
anvgithub <88430353+anvgithub@users.noreply.github.com>
2021-09-17 16:59:48 +0300

    Update and rename variable.tf to variables.tf

--- module/variables.tf
+++ module/variables.tf
@@ -97,7 +97,7 @@ variable "cmd_extension" {
 }
 
 variable "tags" {
-  type        = "map"
+  type        = string (map)
   description = "A map of the tags to use on the resources that are deployed with this module."
 
   default = {
