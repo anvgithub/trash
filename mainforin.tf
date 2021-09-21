43deac0f647efc25adec30d5a682a00dc1342168
anvgithub <88430353+anvgithub@users.noreply.github.com>
2021-09-21 10:11:50 +0300

    Update mainforin.tf

--- mainforin.tf
+++ mainforin.tf
@@ -64,7 +64,7 @@ resource "azurerm_linux_virtual_machine" "example" {
 
   admin_ssh_key {
     username   = "anv"
-    public_key = file("~/.ssh/id_rsa.pub")
+    public_key = file("home/anv/.ssh/id_rsa.pub")
   }
 
   os_disk {
