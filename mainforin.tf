720ab598ca181986f8f1d5cae73e70a5ba76c5df
anvgithub <88430353+anvgithub@users.noreply.github.com>
2021-09-21 12:23:25 +0300

    Update mainforin.tf

--- mainforin.tf
+++ mainforin.tf
@@ -57,7 +57,7 @@ resource "azurerm_linux_virtual_machine" "example" {
   resource_group_name = azurerm_resource_group.example.name
   location            = azurerm_resource_group.example.location
   size                = "Standard_F2"
-  admin_username      = "adminuser"
+  admin_username      = "anv"
   network_interface_ids = [
     azurerm_network_interface.example.id,
   ]
