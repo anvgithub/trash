8b9d35b67e1b15deb0b9de1d1cc353c0f036c406
anvgithub <88430353+anvgithub@users.noreply.github.com>
2021-09-21 12:36:45 +0300

    Update mainforin.tf

--- mainforin.tf
+++ mainforin.tf
@@ -17,22 +17,6 @@ terraform {
     }
 }
 
-
-resource "azurerm_resource_group" "vmss" {
- name     = var.resource_group_name
- location = var.location
- tags     = var.tags
-}
-
-resource "random_string" "fqdn" {
- length  = 6
- special = false
- upper   = false
- number  = false
-}
-
-
-
 resource "azurerm_resource_group" "vmss" {
  name     = var.resource_group_name
  location = var.location
