9350d9d38fbc29eddaef12d351787c2e2d368ebe
anvgithub <88430353+anvgithub@users.noreply.github.com>
2021-09-21 18:08:32 +0300

    Update mainforin.tf

--- mainforin.tf
+++ mainforin.tf
@@ -22,19 +22,6 @@ provider "azurerm" {
 }
 
 
-resource "azurerm_resource_group" "vmss" {
- name     = var.resource_group_name
- location = var.location
- tags     = var.tags
-}
-
-resource "random_string" "fqdn" {
- length  = 6
- special = false
- upper   = false
- number  = false
-}
-
 # Create a resource group if it doesn’t exist
 resource "azurerm_resource_group" "rg" {
   name     = "javademo"
