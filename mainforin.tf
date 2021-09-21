e45919b14a18a397ed3622207508c931b694f2e8
anvgithub <88430353+anvgithub@users.noreply.github.com>
2021-09-21 13:00:22 +0300

    Update mainforin.tf

--- mainforin.tf
+++ mainforin.tf
@@ -181,6 +181,7 @@ resource "azurerm_virtual_machine" "jumpbox" {
  vm_size               = "Standard_DS1_v2"
  admin_username        = "anv"
  network_interface_ids = [azurerm_network_interface.jumpbox.id]
+ disable_password_authentication = true
 
    admin_ssh_key {
     username   = "anv"
