48a8ffc841ba0601b37a489b85bad69c50323d4c
anvgithub <88430353+anvgithub@users.noreply.github.com>
2021-09-21 12:47:53 +0300

    Update mainforin.tf

--- mainforin.tf
+++ mainforin.tf
@@ -179,7 +179,7 @@ resource "azurerm_virtual_machine" "jumpbox" {
  location              = var.location
  resource_group_name   = azurerm_resource_group.vmss.name
  network_interface_ids = [azurerm_network_interface.jumpbox.id]
- size                  = "Standard_DS1_v2"
+ vm_size               = "Standard_DS1_v2"
  admin_username        = "anv"
 
 
