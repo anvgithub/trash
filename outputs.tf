7a02ed7a044fb092615814cf5526aaf7c61e0f5b
anvgithub <88430353+anvgithub@users.noreply.github.com>
2021-09-22 16:05:50 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -2,3 +2,9 @@
   # value = azurerm_public_ip.vmss.fqdn
 #}
 
+  # sh """
+                        export vmss_ip=${terraform output vm_ip}
+                        #echo "host1 ansible_ssh_port=50001 ansible_ssh_host=$vmss_ip" > /home/anv/inventory
+                        #echo "host2 ansible_ssh_port=50002 ansible_ssh_host=$vmss_ip" >> home/anv/inventory
+                        #cat inventory
+                       # """
