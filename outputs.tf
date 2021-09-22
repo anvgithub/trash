829342a0ac240ab60ca902245f490bc9b0809192
anvgithub <88430353+anvgithub@users.noreply.github.com>
2021-09-22 17:58:24 +0300

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,7 +1,7 @@
 output "vm_ip" {
-  value = "${azurerm_public_ip.lbpip.fqdn}"
+  value = ${azurerm_public_ip.lbpip.fqdn}
 }
 
 output "vm_dns" {
-  value = "http://${azurerm_public_ip.lbpip.fqdn}"
+  value = http://${azurerm_public_ip.lbpip.fqdn}
 }
