8207379003a4a51a026db4c7fe8de631ed880e4b
Anatolii Neveselyi <anatolii.neveselyi@gmail.com>
2021-09-23 11:19:34 +0300

    count

--- mainforin.tf
+++ mainforin.tf
@@ -140,6 +140,7 @@ resource "azurerm_network_interface_backend_address_pool_association" "nic_to_ba
   network_interface_id    =["${element(azurerm_network_interface.nic.*.id, count.index)}"]
   ip_configuration_name   = "ipconfig${count.index}"
   backend_address_pool_id = "${azurerm_lb_backend_address_pool.backend_pool.id}"
+  count                 = 2
 }
 
 # Create virtual machine
